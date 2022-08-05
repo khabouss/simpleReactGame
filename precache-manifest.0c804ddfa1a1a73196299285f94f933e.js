6ba1c920fda7615c5215656d1f7208a5c2df1729
Taha Khabouss <tkhabous@e1r13p4.1337.ma>
2022-08-05 23:58:56 +0100

    Updates

---
+++ precache-manifest.0c804ddfa1a1a73196299285f94f933e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe68ea2311911d3b7f6",
+    "url": "/simpleReactGame/static/css/main.01afd76a.chunk.css"
+  },
+  {
+    "revision": "0fe68ea2311911d3b7f6",
+    "url": "/simpleReactGame/static/js/main.0fe68ea2.chunk.js"
+  },
+  {
+    "revision": "ad2b29cdfb06374227e2",
+    "url": "/simpleReactGame/static/js/1.ad2b29cd.chunk.js"
+  },
+  {
+    "revision": "a15a0a588f5e3a7cd155",
+    "url": "/simpleReactGame/static/js/runtime~main.a15a0a58.js"
+  },
+  {
+    "revision": "c947ec9df81fc470c92352d6493a65b4",
+    "url": "/simpleReactGame/index.html"
+  }
+];
